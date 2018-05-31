e19c35abb551e4546dfddecfb230455fb1a68e5d
Kareem Mostafa <kareemmmostafaa@gmail.com>
2018-05-31 22:49:08 +0200

    Add 'Menu changes visibility' SPECS

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -61,6 +61,7 @@ $(function() {
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+
          it('Menu hidden by default', function() {
            expect($('body').hasClass('menu-hidden')).toBe(true);
          });
@@ -70,6 +71,14 @@ $(function() {
           * should have two expectations: does the menu display when
           * clicked and does it hide when clicked again.
           */
+
+          it('Menu changes visibility', function() {
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(false);
+            $('.menu-icon-link').click();
+            expect($('body').hasClass('menu-hidden')).toBe(true);
+          });
+          
     });
     /* TODO: Write a new test suite named "Initial Entries" */
 
