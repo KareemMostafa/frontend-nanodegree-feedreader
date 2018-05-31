4b098e809d38d250ab46f85e9fd51307b0525cc6
Kareem Mostafa <kareemmmostafaa@gmail.com>
2018-06-01 01:49:17 +0200

    Add 'Initial Entries' test suite

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -78,9 +78,14 @@ $(function() {
             $('.menu-icon-link').click();
             expect($('body').hasClass('menu-hidden')).toBe(true);
           });
-          
+
     });
+
     /* TODO: Write a new test suite named "Initial Entries" */
+        describe('Initial Entries', function() {
+          beforeEach(function(done){
+            loadFeed(0,done);
+          });
 
         /* TODO: Write a test that ensures when the loadFeed
          * function is called and completes its work, there is at least
@@ -88,6 +93,10 @@ $(function() {
          * Remember, loadFeed() is asynchronous so this test will require
          * the use of Jasmine's beforeEach and asynchronous done() function.
          */
+         it('minimum number of element', function() {
+           expect($('.feed .entry').length).toBeGreaterThan(0);
+         });
+      });
 
     /* TODO: Write a new test suite named "New Feed Selection" */
 
